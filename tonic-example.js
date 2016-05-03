266b70e3aec4631ac568a5ba0a0bb8cc8ba3714f
Comandeer <vepomoc@gmail.com>
2016-05-03 23:23:30 +0200

    [ci skip] Updated Tonic example.

--- tonic-example.js
+++ tonic-example.js
@@ -1,12 +1,14 @@
+require( 'bemquery-selector-converter/package.json' );
+require( 'bemquery-selector-engine/package.json' );
 require( 'bemquery-core/package.json' );
 
 var fs = require( 'fs' ),
-    url = require( 'url' ),
+	url = require( 'url' ),
 	endpoint = 'https://tonicdev.io' + process.env.TONIC_ENDPOINT_PATH;
 
 exports.tonicEndpoint = function( request, response ) {
-    const package = url.parse( request.url, true ).query.package;
-    
+	const package = url.parse( request.url, true ).query.package;
+
 	response.end( fs.readFileSync( require.resolve( 'bemquery-' + package ) ) );
 };
 
@@ -20,10 +22,16 @@ exports.tonicEndpoint = function( request, response ) {
 		</head>
 		<body>
 			<p>Example</p>
-			
-            <script src="${endpoint}?package="></script>
-			<script>
 
+			<script src="${endpoint}?package=selector-converter"></script>
+			<script src="${endpoint}?package=selector-engine"></script>
+			<script src="${endpoint}?package=core"></script>
+			<script>
+				const bemQuery = bemquery.default( 'block element' );
+
+				bemQuery.each( ( elem ) => {
+					console.log( elem );
+				} );
 			</script>
 		</body>
 	</html>`;
